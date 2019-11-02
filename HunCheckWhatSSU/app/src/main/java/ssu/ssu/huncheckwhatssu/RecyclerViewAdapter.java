c1054a929ba3c80e0024700b6c5a45b0bb41d75d
Eundms <nab05184@gmail.com>
2019-11-02 14:05:22 +0900

    리사이클러뷰 (without firebase)버전

---
+++ HunCheckWhatSSU/app/src/main/java/ssu/ssu/huncheckwhatssu/RecyclerViewAdapter.java
@@ -0,0 +1,62 @@
+package ssu.ssu.huncheckwhatssu;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    private ArrayList<Book> mData = null ;
+
+    // 아이템 뷰를 저장하는 뷰홀더 클래스.
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textView1 ;
+        TextView textView2;
+        TextView textView3;
+        ViewHolder(View itemView) {
+            super(itemView) ;
+
+            // 뷰 객체에 대한 참조. (hold strong reference)
+            textView1 = itemView.findViewById(R.id.text1) ;
+            textView2 = itemView.findViewById(R.id.text2) ;
+            textView3 = itemView.findViewById(R.id.text3) ;
+        }
+    }
+
+    // 생성자에서 데이터 리스트 객체를 전달받음.
+    RecyclerViewAdapter(ArrayList<Book> list) {
+        mData = list ;
+    }
+
+    // onCreateViewHolder() - 아이템 뷰를 위한 뷰홀더 객체 생성하여 리턴.
+    @Override
+    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext() ;
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) ;
+
+        View view = inflater.inflate(R.layout.recyclerview_item, parent, false) ;
+        RecyclerViewAdapter.ViewHolder vh = new RecyclerViewAdapter.ViewHolder(view) ;
+
+        return vh ;
+    }
+
+    // onBindViewHolder() - position에 해당하는 데이터를 뷰홀더의 아이템뷰에 표시.
+    @Override
+    public void onBindViewHolder(RecyclerViewAdapter.ViewHolder holder, int position) {
+
+        holder.textView1.setText(mData.get(position).getTitle());
+        holder.textView2.setText(mData.get(position).getPrice());
+        holder.textView3.setText(mData.get(position).getSeller());
+    }
+
+    // getItemCount() - 전체 데이터 갯수 리턴.
+    @Override
+    public int getItemCount() {
+        return mData.size() ;
+    }
+}
--- HunCheckWhatSSU/app/src/main/java/ssu/ssu/huncheckwhatssu/TradeFragment.java
+++ HunCheckWhatSSU/app/src/main/java/ssu/ssu/huncheckwhatssu/TradeFragment.java
@@ -8,8 +8,12 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
 
 public class TradeFragment extends Fragment {
 
@@ -22,6 +26,25 @@ public class TradeFragment extends Fragment {
         BottomNavigationView navView = navHostFragment.getActivity().findViewById(R.id.nav_view);
         Menu menu = navView.getMenu();
         menu.getItem(2).setChecked(true);
+
+        /* ArrayList<String> list = new ArrayList<>();
+        for (int i=0; i<100; i++) {
+            list.add(String.format("TEXT %d", i)) ;
+        } */
+
+        ArrayList<Book>list =new ArrayList<Book>();
+        list.add(new Book("git","10000","dms"));
+        list.add(new Book("hi","300","dagaja"));
+        list.add(new Book("123hi","4300","dagajdfa"));
+        list.add(new Book("dgfhi","2300","dagajdfdfa"));
+        list.add(new Book("sfshi","2323300","dagajdfdfa"));
+        // 리사이클러뷰에 LinearLayoutManager 객체 지정.
+        RecyclerView recyclerView = root.findViewById(R.id.list) ;
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext())) ;
+
+        // 리사이클러뷰에 SimpleTextAdapter 객체 지정.
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(list) ;
+        recyclerView.setAdapter(adapter) ;
 
         return root;
     }
