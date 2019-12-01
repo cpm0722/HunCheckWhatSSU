70201f119bfb4c59da113d63396fef28d129d419
Seoungjoo <seoungjoo01@naver.com>
2019-12-02 06:28:14 +0900

    Rating activity

--- HunCheckWhatSSU/app/src/main/java/ssu/ssu/huncheckwhatssu/Rating.java
+++ HunCheckWhatSSU/app/src/main/java/ssu/ssu/huncheckwhatssu/Rating.java
@@ -3,6 +3,8 @@ package ssu.ssu.huncheckwhatssu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class Rating extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class Rating extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
+        final TextView textView=(TextView)findViewById(R.id.rateMessage);
+        RatingBar rb=(RatingBar)findViewById(R.id.ratingBar);
+        rb.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                textView.setText("평가점수:"+rating);
+            }
+        });
     }
 }
