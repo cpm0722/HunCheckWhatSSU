4e44738745602899a6c7e9e8e23aa76248147ff2
Seoungjoo <seoungjoo01@naver.com>
2019-12-02 07:11:51 +0900

    Rating activity

--- HunCheckWhatSSU/app/src/main/java/ssu/ssu/huncheckwhatssu/Rating.java
+++ HunCheckWhatSSU/app/src/main/java/ssu/ssu/huncheckwhatssu/Rating.java
@@ -3,6 +3,8 @@ package ssu.ssu.huncheckwhatssu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -14,10 +16,17 @@ public class Rating extends AppCompatActivity {
         setContentView(R.layout.activity_rating);
         final TextView textView=(TextView)findViewById(R.id.rateMessage);
         RatingBar rb=(RatingBar)findViewById(R.id.ratingBar);
+        Button bt=(Button)findViewById(R.id.fin);
         rb.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
                 textView.setText("평가점수:"+rating);
+            }
+        });
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
