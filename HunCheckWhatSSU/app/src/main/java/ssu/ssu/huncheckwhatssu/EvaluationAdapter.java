0b091ac7bef3cb11197869c690b63f665cfcfca3
Hansu Kim <cpuman7@gmail.com>
2019-12-10 00:19:48 +0900

    add my evaluation tab && report button

--- HunCheckWhatSSU/app/src/main/java/ssu/ssu/huncheckwhatssu/EvaluationAdapter.java
+++ HunCheckWhatSSU/app/src/main/java/ssu/ssu/huncheckwhatssu/EvaluationAdapter.java
@@ -1,6 +1,10 @@
 package ssu.ssu.huncheckwhatssu;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,8 +12,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -18,11 +24,14 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.nikhilpanju.recyclerviewenhanced.RecyclerTouchListener;
 
 import java.util.Vector;
 
 import ssu.ssu.huncheckwhatssu.utilClass.Customer;
 import ssu.ssu.huncheckwhatssu.utilClass.Trade;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class EvaluationAdapter extends RecyclerView.Adapter<EvaluationAdapter.MyViewHolder> {
 
@@ -30,17 +39,19 @@ public class EvaluationAdapter extends RecyclerView.Adapter<EvaluationAdapter.My
     private Vector<Trade> trades;
     private LayoutInflater layoutInflater;
     private String id;
-    private FirebaseCommunicator firebaseCommunicator;
+    private boolean isMyId;
 
-    public interface OnItemClickListener{
+    public interface OnItemClickListener {
         void onItemClicked(View v, int position);
     }
-    public EvaluationAdapter(Context context, Vector<Trade> doneTrades, String id){
+
+    public EvaluationAdapter(Context context, Vector<Trade> doneTrades, String id, boolean isMyId) {
         this.trades = doneTrades;
 
         Log.d(TAG, "EvaluationAdapter: last" + trades.size());
         this.layoutInflater = LayoutInflater.from(context);
         this.id = id;
+        this.isMyId = isMyId;
     }
 
     @NonNull
@@ -54,13 +65,12 @@ public class EvaluationAdapter extends RecyclerView.Adapter<EvaluationAdapter.My
     @Override
     public void onBindViewHolder(@NonNull final MyViewHolder holder, int position) {
         String counterpartId = null;
-        if(trades.get(position).getSellerId().equals(id)){
+        if (trades.get(position).getSellerId().equals(id)) {
             holder.isSell.setText("판매");
             counterpartId = this.trades.get(position).getPurchaserId();
             holder.rating.setText(this.trades.get(position).getSellerRate() + "");
             holder.comment.setText(this.trades.get(position).getSellerComment());
-        }
-        else{
+        } else {
             holder.isSell.setText("구매");
             counterpartId = this.trades.get(position).getSellerId();
             holder.rating.setText(this.trades.get(position).getPurchaserRate() + "");
@@ -79,16 +89,21 @@ public class EvaluationAdapter extends RecyclerView.Adapter<EvaluationAdapter.My
         }));
     }
 
+    public Vector<Trade> getTrades() {
+        return trades;
+    }
+
     @Override
     public int getItemCount() {
         return trades.size();
     }
 
-    public class MyViewHolder extends  RecyclerView.ViewHolder{
+    public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView isSell;
         public TextView fromId;
         public TextView rating;
         public TextView comment;
+
         public MyViewHolder(@NonNull final View itemView) {
             super(itemView);
             this.isSell = itemView.findViewById(R.id.item_evaluation_isSell);
@@ -97,4 +112,50 @@ public class EvaluationAdapter extends RecyclerView.Adapter<EvaluationAdapter.My
             this.comment = itemView.findViewById(R.id.item_evaluation_text_comment);
         }
     }
+
+    //RecyclerView에 TouchListener 설정 함수 (Swipe로 메뉴 출력 가능하게)
+    public void setSwipeable(final Context context, final Activity activity, final RecyclerView recyclerView) {
+        final RecyclerTouchListener onTouchListener = new RecyclerTouchListener(activity, recyclerView);
+        onTouchListener
+                .setClickable(new RecyclerTouchListener.OnRowClickListener() {
+                    @Override
+                    public void onRowClicked(int position) {
+                    }
+
+                    @Override
+                    public void onIndependentViewClicked(int independentViewID, int position) {
+                    }
+                })
+                .setSwipeOptionViews(R.id.item_evaluation_button_report)
+                .setSwipeable(R.id.rowFG, R.id.rowBG, new RecyclerTouchListener.OnSwipeOptionsClickListener() {
+
+                    @Override
+                    public void onSwipeOptionClicked(int viewID, final int position) {
+                        final Trade trade = ((EvaluationAdapter) (recyclerView.getAdapter())).getTrades().get(position);
+                        if (viewID == R.id.item_evaluation_button_report) {
+                            AlertDialog.Builder alert = new AlertDialog.Builder(context);
+                            alert.setTitle("신고");
+                            alert.setMessage("정말로 해당 평가에 대해 신고를 하시겠습니까? 허위 신고는 제재 사유입니다.");
+                            alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    FirebaseDatabase.getInstance().getReference().child("report").child(trade.getTradeId()).setValue(FirebaseCommunicator.getMyId());
+                                    Toast.makeText(context, "신고 접수 완료! 관리자가 해당 신고에 대해 검토합니다.", Toast.LENGTH_LONG).show();
+                                }
+                            });
+                            alert.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface arg0, int arg1) {
+                                }
+                            });
+                            alert.show();
+                        }
+
+                    }
+                });
+        if(isMyId)
+            recyclerView.addOnItemTouchListener(onTouchListener);
+        return;
+    }
+
 }
